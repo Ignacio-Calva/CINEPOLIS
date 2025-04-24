f89d048031f2be511e7c238fe4f8874977e091b8
abyy15 <abriljuanatrinidad@gmail.com>
2025-04-24 19:30:20 -0300

    BASE

---
+++ CINEPOLIS/Funcion.h
@@ -0,0 +1,16 @@
+#ifndef PELICULAS_H
+#define PELICULAS_H
+
+
+class Peliculas
+{
+    public:
+        Peliculas() {}
+        virtual ~Peliculas() {}
+
+    protected:
+
+    private:
+};
+
+#endif // PELICULAS_H
